b46558c5bdbb171faf77e1c602dc694b2541d085
AhnafK <akazi1@stuy.edu>
2017-11-28 14:58:07 -0500

    Created 05Library

---
+++ ReversedCharSeq/ReveredCharSeq.java
@@ -0,0 +1,24 @@
+public class ReversedCharSeq implements CharSequence{
+    String wgvbhjmn;
+    
+    public ReversedCharSeq(String chars){
+	wgvbhjmn=""
+	for(int x = chars.length()-1; x >= 0; x++){
+	    wgvbvjmn += chars.charAt(x);
+	}
+    }
+
+    public char charAt(int index){
+	return charAt(wgvbhjmn);
+    }
+
+    public int length(){
+	return wgvbhjmn.length();
+    }
+
+    
+    
+    public String toString(){
+	return wgbhjmn;
+    }
+}

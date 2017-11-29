f36dbd7a8a7cef306c65be76ab9d9e066f2566b0
Ahnaf Kazi <akazi1@stuy.edu>
2017-11-28 21:35:39 -0500

    did a thing: added abstracts and the shell of a compareto

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -6,5 +6,19 @@ public abstract class LibraryBook extends Book implements Comparable<LibraryBook
 	callNumber = callN;
     }
 
-    
+    public void setCallNumber(String callN){
+	callNumber = callN;
+    }
+
+    public String getCallNumber(){
+	return callNumber;
+    }
+
+    public abstract void checkout(String patron, String due);
+    public abstract void returned();
+    public abstract String circulationStatus();
+
+    public int compareTo(LibraryBook other){
+	compareTo(other);
+    }
 }

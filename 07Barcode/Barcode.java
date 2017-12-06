275b88f1686e6b32bd75798b0b35b9a2055e7ddf
Ahnaf Kazi <akazi1@stuy.edu>
2017-12-05 22:12:06 -0500

    COmpleted, I think

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,6 +1,6 @@
 public class Barcode implements Comparable<Barcode>{
     private String zip;
-    
+    private static String[] code = new String[] {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
     public Barcode(String z){
 	killBadZip(z);
 	zip = z;
@@ -19,12 +19,28 @@ public class Barcode implements Comparable<Barcode>{
     public String getZip(){
 	return zip;
     }
-
-    public static String toBarcode(int d){
-	String[] code = new String[] {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
-	    return code[d];
+    
+    public static String toZip(String cod){
+	if(cod.length()!=32 || cod.charAt(0) != '|' || cod.charAt(31)!='|'){
+	    throw new IllegalArgumentException();
+	}
+	String t = "";
+	for(int x = 1; x < 31; x+=5){
+	    String current = cod.substring(x,x+5);
+	    for(int y = 0; y<code.length; y++){
+		if (current.equals(code[y])){
+		    t += y + "";
+		    break;
+		}
 	    }
+	}
+	if(Integer.parseInt(t.substring(0,5))%10 != Character.getNumericValue(t.charAt(5))){
+	    throw new IllegalArgumentException();
+	}
+	return t.substring(0,5);
+    }
 
+		    
     public String getCode(){
 	return toCode(zip);
     }
@@ -35,10 +51,10 @@ public class Barcode implements Comparable<Barcode>{
 	int sum = 0;
 	for(int x = 0; x < zip.length(); x++){
 	    int current = Character.getNumericValue(zip.charAt(x));
-	    B += toBarcode(current);
+	    B += code[current];
 	    sum+=current;
 	}
-	B+=toBarcode(sum%10)+"|";
+	B+=code[sum%10]+"|";
 	return B;
     }
 
@@ -55,5 +71,6 @@ public class Barcode implements Comparable<Barcode>{
 	System.out.println(g.getCode());
 	System.out.println(g.getZip());
 	System.out.println(g);
+	System.out.println(Barcode.toZip(g.getCode()));
     }
 }

64ec0451aff8561beba882f79f1e4710ff54bf09
AhnafK <akazi1@stuy.edu>
2017-12-13 15:30:15 -0500

    file

---
+++ 09GUI/09GUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.events.*;
+
+public class CF extends JFrame{
+    private Container pane;
+    private JCheckBox c;
+    private JTtextField t;
+    
+    public CF(){
+	this.setTitle("Farenheight to Celcius");
+	this.setSize(700,700);
+	this.setLocation(30,50);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	pane = this.getContentPane();
+
+	c = new JCheckBox("revers?");
+	t = new JTextField(234789);
+
+	pane.add(c);
+	pane.add(t);
+    }
+      
+}
